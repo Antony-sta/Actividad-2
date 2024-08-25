b831f7d825d59eb14613e318e0280f5b26dac005
jburelolainez@gmail.com <jburelolainez@gmail.com>
2024-08-24 20:25:37 -0600

    Codigos

---
+++ 02-Repaso.js
@@ -0,0 +1,32 @@
+// realizar un areglo con dos dimenciones y guardar dos arreglos internos y dar lectura a los internos 
+let A=[[3,4,5,6],[7,8,9]];
+    
+const datosArreglo=()=>{
+A.map((datos,index)=>{
+  datos.map((valores,i)=>{
+
+    console.log(`${i}=${valores}`);
+  })
+});
+  
+}
+    
+
+// for of 
+
+// for in 
+// funcion flecha 
+// while
+// for each 
+
+// Estructura de control parser
+let a=4;
+let b=5;
+
+if(a>b){
+  console.log(`La a es mayor ${a}`);
+
+}else{
+  console.log(`La b es mayor ${a}`);
+}
+
