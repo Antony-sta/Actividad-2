760e4a9f7c7b07902ea9bb46522b2f933bd36b87
jburelolainez@gmail.com <jburelolainez@gmail.com>
2024-09-13 18:01:28 -0600

    CONTEXTO

--- T1-E-JesusExam.js
+++ T1-E-JesusExam.js
@@ -57,6 +57,8 @@ const cliente = new client();
 cliente.RegistrClient()
 cliente.EliminDeudor() */
 
+const { Client } = require("undici-types");
+
 // Control de Películas Registro de películas (número de película, titulo, clasificación, estado: disponible o rentada, tipo: estreno o catálogo). 
 /* const prompt = require ("prompt-sync")()
 
@@ -127,53 +129,158 @@ star.Eliminarpeli(); */
 const prompt = require("prompt-sync")()
 
 class Cliente {
-    constructor(Membresia, Nombre, Direccion, Telefono, Estado) {
+    constructor(Membresia, Nom, Direcc, Telefono, Estad) {
         this._membresia = Membresia;
-        this._nombre = Nombre;
-        this._direccion = Direccion;
+        this._Nom = Nom;
+        this._Direcc = Direcc;
         this._telefono = Telefono;
-        this._estado = Estado;
+        this._Estad = Estad;
         this._consulta = []
 
         registroCliente() {
-            let valorar = parseInt(prompt("Ingrese la cantidad de personas para registrar: "));
-            for (let i = 0; i < valorar; i++) {
-                this._nombre = prompt("Ingrese su nombre: ");
-                this._estado = prompt(`Cual es tu estado (1. Deudor), (2. libre )`) 
-                switch (parseInt(this._estado)) {
+            let valor = parseInt(prompt("Cantidad de personas para registrar: "));
+            for (let i = 0; i < valor; i++) {
+                this._Nom = prompt(" Ingrese Nombre: ");
+                this._Estad = prompt(`Cual es tu estado (1. Endeudado), (2. Libre de Membresia )`);
+
+                switch (parseInt(this._Estad)) {
                     case 1:
-                        this._estado = 'Deudor';
+                        this._estado = 'Endeudado';
                         break;
                     case 2:
-                        this._estado = 'Libre';
+                        this._estado = 'Libre de Membresia';
                         break;
                 }
+        }
+
+    
+let datos = {
+    Nom: this._Nom,
+    Membresia: this._Membresia,
+    Direcc: this._Direcc,
+    telefono: this._telefono,
+    Estad: this._Estad
+}
+this._consulta.push(datos)
+ }
+}
+
+eliminarDeudor() {
+for (let i = 0; i < this._consulta.length; i++) {
+if (this._consulta[i].Estad === 'Endeudado') {
+    this._consulta.splice(i, 1);
+    console.log(`Cliente ${this._consulta[i].Nom} tu membrecia has sido dado de baja `);
+
+} else {
+    console.log(`Cliente ${this._consulta[i].Nom} con membrecia sin libre de multa `);
+   }
+  }
+ }
+}
+
+class cine extends Client{
+    constructor (Num, Titul, Clasificacion, Disponib, Tip) {
+        super()
+        this._Num = Num
+        this._titul = titul
+        this._Clasificacio = Clasificacion
+        this._Disponi = Disponi
+        this._tip = tip
+        this._consulta = []
+        this._preciopeli = 0
+    }
+    // control de pelis
+    consultaPelis(){
+        const peli = parseInt(prompt("Peliculas a consultar"));
+        for (let i = 0; i < peli; i++) {
+            this._titul = prompt("Nombre de la pelicula");
+            this._Disponi = parseInt(prompt("Aun hay (1. Disponible),(2. Rentando)"));
+            switch (parseInt(this._Disponi)){
+                case 1:
+                    this._Disponi = `Disponible`
+                    break
+                case 2:
+                    this._Disponi = `Rent`
+                    break
+                case 3:
+                    this._tip = `Estreno`
+                    break
+                case 4:
+                    this._tip = `Catalog`
+                    break
             }
+            const vare = {
+                Num: this._Num,
+                titul: this._titul,
+                Disponi: this._Disponi,
+                tip: this._tip,
+                Clasificacion: this._Clasificacion,
+            }
+            this._consulta.push(vare)
+        }
+   }
+
+   EliminarPeli() {
+    for (let i =0; i < this._consulta.length; i++) {
+        if (this._consulta[i].Disponi === `Rent`) {
+            this._consultar.splice(i, 1);
+
+        } else{
+            console.log(`La Pelicula ${this._consulta[i].titul} esta disponible`);
         }
     }
 }
 
+rentPeli(titulo, vare) {
+    const cinemex = this._consulta;
+    const peliencontrada = null;
+
+for (let i = 0; i< cinemex.length; i++) {
+    if(cinemex[i].titul === titul) {
+        peliencontrada = cinemex[i]
+        break;
+    }
 }
-let datos = {
-    nombre: this._nombre,
-    membresia: this._membresia,
-    direccion: this._direccion,
-    telefono: this._telefono,
-    estado: this._estado
-}
-this._consulta.push(datos)
-}
+if(peliencontrada) {
+    if (peliencontrada.Disponi ===`Disponibl`) {
+        let deudorLibre = verif._consulta.some(c => c.Estad === `Endeudado`)
+        if (deudorLibre) {
+            console.log("salde su deuda");
+        }else{
+            if (peliencontrada.tip === `Estreno`) {
+                this._preciopeli = 50;
+            } else if (peliencontrada.tip === `catalog`) {
+                this._preciopeli = 35
+            }
+        const FechaRent = prompt("Ingresar la fecha de renta")
+        const Fechalimite = this.calcularlimite(FechaRent)
+        peliencontrada.dispo = 'Rentata';
+        peliencontrada.fechaRenta = fechaRenta;
+        peliencontrada.fechaLimite = fechaLimite;
+        console.log(`La pelicula ${titulo} se ha rentado`);
+        console.log(`Fecha limite de devolucion${peliencontrada.FechaRent}`);
+        console.log(`El precio es ${this._preciopeli}`);
+        }
+    } else {
+        console.log("Pelicula no disponible");
+    }
+  }
 }
 
+calcularlimite(FechaRent){
+    const Fechalimite = new Date(FechaRent);
+    Fechalimite.setDate(Fechalimite.getDate() + 3)
+    return Fechalimite.toISOString().slice(0, 10)
+  }
+}
 
-eliminarDeudor() {
-for (let i = 0; i < this._consulta.length; i++) {
-if (this._consulta[i].estado === 'Deudor') {
-    this._consulta.slice(i, 1);
-    console.log(`Cliente ${this._consulta[i].nombre} con membrecia  has sido dado de baja `);
+const client = Client();
+Cliente.registroCliente();
+client.eliminarDeudor();
 
-} else {
-    console.log(`Cliente ${this._consulta[i].nombre} con membrecia sin libre de multa `);
-}
-}
-}
+const sterci = new cine ();
+sterci._consultar 
+const titulo = prompt("Ingrese el titulo de la peli a rentar ")
+const verif = client
+star.renrarPeli(titulo, verif)
+star.eliminarPeli();
